ccc33156aeb8caecc06584ebcb2f147b27452333
Meenakshi S <96098872+myself-meen@users.noreply.github.com>
2026-05-07 10:30:16 +0000

    Add About section content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Home from './Components/Home'
+import About from './Components/About'
+import Services from './Components/Services'
+import Contact from './Components/Contact'
+import Navbar from './Components/Navbar';
+
+
 function App() {
   return (  <>
-  
+  <>
+  <Navbar/>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/services" element={<Services />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="*" element={<Error />} />
+  </Routes>
+  </>
+
   </>);
 }
 
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 function About() {
     return ( <>
+    <h2>About Us</h2>
+    <div style={{display:'flex', flexDirection:'row', justifyContent:'space-around'}}>
+    <section id='vision'>
+        <h3>Our Vision</h3>
+        <p> To revolutionize how businesses connect with their audiences by creating lasting, human-centered digital experiences.</p>
+
+    </section>
+    <section id='mission'>
+        <h3>Our Mission</h3>
+        <p>To deliver exceptional ROI by transforming digital challenges into creative, measurable, and impactful solutions.</p>
+
+    </section>
+    <section id='values'>
+        <h3>Our Values</h3>
+        <p>To foster authentic digital connections between brands and consumers through transparency, creativity, and cutting-edge technology.</p>
+
+    </section>
+    </div>
     </> );
 }
 
