4cea36a80fe8af2aee8afb4879ca833f06f2bbb3
Meenakshi S <96098872+myself-meen@users.noreply.github.com>
2026-05-07 10:32:53 +0000

    Add Navbar Section content

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/vite.svg';
 function Navbar() {
     return ( <>
+    <nav style={{display:'flex', columnGap:'100px', justifyContent:'space-between'}}>
+        <div style={{display:'flex',columnGap:'5px'}}>
+        <img id='logo' src={logo} alt='logo' style={{width:'40px', height:'40px', alignSelf:'center'}}/>
+        <h3>SM Digital Marketing</h3>
+        </div>
+        <div style={{display:'flex', columnGap:'10px', justifyContent:'space-between', alignSelf:'center'}}>
+        <Link to='/'>Home</Link> |{" "}
+        <Link to='/about'>About</Link> |{" "}
+        <Link to='/services'>Services</Link> |{" "}
+        <Link to='/contact'>Contact</Link>
+        </div>
+    </nav>
+
     </>
      );
 }
