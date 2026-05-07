ee17c39efcb5a975b7dfe1ba4e033b16fae0546b
Meenakshi S <96098872+myself-meen@users.noreply.github.com>
2026-05-07 10:46:28 +0000

    Add the overall website content by import other components

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,27 @@ import About from './Components/About'
 import Services from './Components/Services'
 import Contact from './Components/Contact'
 import Navbar from './Components/Navbar';
-
+import Footer from './Components/Footer';
 
 function App() {
-  return (  <>
-  <>
-  <Navbar/>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/services" element={<Services />} />
-    <Route path="/contact" element={<Contact />} />
-    <Route path="*" element={<Error />} />
-  </Routes>
-  </>
-
-  </>);
+  return (
+    <>
+    <section style={{padding:'30px'}}>
+      <Navbar />
+      </section>
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    
+      <section style={{backgroundColor:'dodgerblue',padding:'30px'}}>
+      <Footer />
+      </section>
+    </>
+  )
 }
 
 export default App;
