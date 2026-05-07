e46e9ab577e1a96bd9f6ac84e11499730573cd2f
Meenakshi S <96098872+myself-meen@users.noreply.github.com>
2026-05-07 10:34:47 +0000

    Add Service component content

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
+import seo from '../assets/seo.jpg'
+import smm from '../assets/social_media_marketing.jpg'
+import cm from '../assets/content_marketing.jpg'
 function Services() {
     return ( <>
+    <h2>Our Services</h2>
+    <div style={{display:'flex', flexDirection:'row', justifyContent:"space-around"}}>
+        <section>
+            <img src={seo} alt="SEO" style={{width:'200px', height:'200px'}}/>
+            <h3>Search Engine Optimization</h3>
+            <p></p>
+        </section>
+        <section>
+            <img src={smm} alt="SMM" style={{width:'200px',height:'200px'}}/>
+            <h3>Social Media Management</h3>
+            <p></p>
+        </section>
+        <section>
+            <img src={cm} alt="CM" style={{width:'200px',height:'200px'}}/>
+            <h3>Content Marketing</h3>
+            <p></p>
+        </section>
+    </div>
     </> );
 }
 
