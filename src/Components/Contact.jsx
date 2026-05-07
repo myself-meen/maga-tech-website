bd46c0d6ef634b1f9af4793b88abdb422dcdc5d0
Meenakshi S <96098872+myself-meen@users.noreply.github.com>
2026-05-07 05:28:07 +0000

    Create and add Files for required components

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+function Contact() {
+    return ( <>
+    </> );
+}
+
+export default Contact;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+function Footer() {
+    return ( <>
+    </> );
+}
+
+export default Footer;
