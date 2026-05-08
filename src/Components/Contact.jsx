411b5c834ed258d17fcf3adf554a90f453ecff59
Meenakshi S <96098872+myself-meen@users.noreply.github.com>
2026-05-08 05:46:19 +0000

    Modify contact section to enable better styling

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,48 +1,72 @@
 import React, { Component } from 'react';
 import { useState } from 'react';
+import { useRef } from 'react';
+import './Contact.css';
 function Contact() {
-    const[name, setName] = useState('')
-    const[email, setEmail] = useState('')
-    const[company, setCompany] = useState('')
-    const[phno, setPhno] = useState('')
-    const[message, setMessage] = useState('')
-    // handleSubmit=(e)=>{
-    //     e.prevent.default;
+  const nameRef=useRef();
+  const emailRef=useRef();
+  const companyRef=useRef();
+  const phnoRef=useRef();
+  const messageRef=useRef();
+
+   const handleSubmit=(e)=>{
+        e.prevent.default;
+        console.log(nameRef.current.value);
+        console.log(emailRef.current.value);
+        console.log(companyRef.current.value);
+        console.log(phnoRef.current.value);
+        console.log(messageRef.current.value);
+        ī
         
 
-    // }
+    }
     
     return ( <>
+     <section id="contact" className="section-panel">
+      
+    <div className="contact-info">
     <h2 className="section-title">Contact Us</h2>
-    <h3>Let's talk</h3>
-    <form className="contact-form">
+    <ul>
+        <li>sgmarketing@info.com</li>
+        <li>321-221-231</li>
+    </ul>
+    </div>
+    <div className="contact-form">
+    <form  onSubmit={handleSubmit} >
+        <div className="customer-info">
+       
         <div className="form-row form-row-space-between">
-            <label htmlFor='name'>Name: </label>
-            <input type="text" id='name' />
+            <label htmlFor='name'>Name </label>
+            <input ref={nameRef} type="text" id='name' />
         
         </div>
          <div className="form-row form-row-space-between">
-            <label htmlFor='email'>Email: </label>
-            <input type="email" id='email' />
+            <label htmlFor='email'>Email </label>
+            <input ref={emailRef} type="email" id='email' />
         
         </div>
+
+
          <div className="form-row form-row-space-between">
-            <label htmlFor='company'>Company: </label>
-            <input type="text" id='company' />
+            <label htmlFor='company'>Company </label>
+            <input ref={companyRef} type="text" id='company' />
             
         </div>
          <div className="form-row form-row-space-between">
-            <label htmlFor='phno'>Ph no:</label>
-            <input type="text" id='phno' />
+            <label htmlFor='phno'>Ph no</label>
+            <input ref={phnoRef} type="text" id='phno' />
             
+        </div>
         </div>
          <div className="form-row">
-            <label htmlFor='message'>Message: </label>
-            <textarea id='message'></textarea>
+            <label htmlFor='message'>Message </label>
+            <textarea ref={messageRef} type="text" id='message' style={{height:'150px'}}></textarea>
             
         </div>
-        <button>Submit</button>
+        <button className='submit-button'>Submit</button>
     </form>
+    </div>
+    </section>
 
     </> );
 }
