d850868450d072b8da79082b7ba0884a37d08828
Meenakshi S <96098872+myself-meen@users.noreply.github.com>
2026-05-12 04:32:50 +0000

    Remove styling via external CSS

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../assets/Logo2.png';
 import { Link } from 'react-router-dom';
-import './Footer.css';
 function Footer() {
     return ( <>
     <section className="footer-section-panel">
