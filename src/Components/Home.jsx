1036f79a8c25f4014c020aae20932735e29325e3
Meenakshi S <96098872+myself-meen@users.noreply.github.com>
2026-05-12 04:33:55 +0000

    Make About and Home section responsive

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,25 +3,23 @@ import About from './About';
 import Services from './Services';
 import Contact from './Contact';
 import Footer from './Footer';
-import './Home.css';
-import  homeimg from '../assets/social-media.svg';
-import '/home/meenakshi/repos/maga-tech-website/src/App.css';
+import homeimg from '../assets/social-media.svg';
 
 function Home() {
   return (
     <>
-      <section id="home" className='home-section-panel'>
-        <div className='home-content'>
-        <h1>Build, Grow and Scale your Brand</h1>
-        <h3>Designing Powerful Online Presence</h3>
+      <section id="home" className='home-section-panel flex flex-col md:flex-row justify-around items-stretch bg-[#F5F7FA] p-7 max-w-7xl mx-auto '>
+        <div className='home-content flex flex-col justify-center items-start w-full md:w-[70%] space-y-4 md:p-16'>
+          <h1 className='text-5xl font-semibold'>Build, Grow and Scale your Brand</h1>
+          <h3 className='font-medium '>Designing Powerful Online Presence</h3>
         </div>
-        <div className='home-image-container'>
-        <img src={homeimg} className='home-image'/>
+        <div className='home-image-container w-full md:w-[40%] flex justify-center items-center  '>
+          <img src={homeimg} className='home-image w-full max-w-full object-contain' alt='Social media h-1 md:h-full' />
         </div>
       </section>
-      <About/>
-      <Services/>
-     <Contact/>
+      <About />
+      <Services />
+      <Contact />
       {/* <section id='footer'><Footer/></section> */}
     </>
   )
